353f9589b3ce450ab877fd5048df7818aa2928d8
Ngtnua206 <ngttu2006@gmail.com>
2026-07-22 09:27:44 +0700

    update

---
+++ CarProject/CarProject/Migrations/20260721105859_AddDongXeAnhAndBannerFields.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDongXeAnhAndBannerFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DuongDanVideo",
+                table: "QuangCaoBanner",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LoaiBanner",
+                table: "QuangCaoBanner",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ThoiGianChuyen",
+                table: "QuangCaoBanner",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DuongDanAnh",
+                table: "DongXe",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DuongDanVideo",
+                table: "QuangCaoBanner");
+
+            migrationBuilder.DropColumn(
+                name: "LoaiBanner",
+                table: "QuangCaoBanner");
+
+            migrationBuilder.DropColumn(
+                name: "ThoiGianChuyen",
+                table: "QuangCaoBanner");
+
+            migrationBuilder.DropColumn(
+                name: "DuongDanAnh",
+                table: "DongXe");
+        }
+    }
+}
